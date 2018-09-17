962e0e8062817f9be09cbff1fafedfde383fba3c
Martin de Frutos <martindfru@gmail.com>
2018-09-17 09:52:13 +0200

    f

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,38 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
+
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity( elem, {
+  // options
+  imagesLoaded: true,
+  cellAlign: 'center',
+  contain: true
+});
+
+var imgs = document.querySelectorAll(".card__image img");
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ? 'transform' : 'WebkitTransform';
+var info = document.querySelector(".info");
+
+flkty.on( 'scroll', function () {
+  flkty.slides.forEach( function (slide, i) {
+    var img = imgs[i];
+    var x = ( slide.target + flkty.x ) * -1/18;
+
+    img.style[transformProp] = 'translateX(' + x + 'px) scale(1.1)';
+  } );
+} );
