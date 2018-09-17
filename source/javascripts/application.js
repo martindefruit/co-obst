77d1a6758b71f28774a5466c7dcb8d468b238e56
Martin de Frutos <martindfru@gmail.com>
2018-09-17 10:02:02 +0200

    sss

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,18 +2,7 @@
 //= require bootstrap
 //= require_tree .
 
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
 
 var elem = document.querySelector('.carousel');
 var flkty = new Flickity( elem, {
