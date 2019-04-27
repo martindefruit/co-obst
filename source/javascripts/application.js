3058fb25a80bed417b572c3e0ec2d483de1c0888
Martin de Frutos <martindfru@gmail.com>
2019-04-27 09:29:50 +0200

    landing

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -13,98 +13,100 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 // TextScramble
 // ——————————————————————————————————————————————————
 
-function textScramble() {
-  var TextScramble = function () {
-    function TextScramble(el) {
-      _classCallCheck(this, TextScramble);
+// function textScramble() {
+//   var TextScramble = function () {
+//     function TextScramble(el) {
+//       _classCallCheck(this, TextScramble);
 
-      this.el = el;
-      this.chars = 'lkjsbdkjbdslkdjsblkbsdl';
-      this.update = this.update.bind(this);
-    }
+//       this.el = el;
+//       this.chars = 'lkjsbdkjbdslkdjsblkbsdl';
+//       this.update = this.update.bind(this);
+//     }
 
-    TextScramble.prototype.setText = function setText(newText) {
-      var _this = this;
+//     TextScramble.prototype.setText = function setText(newText) {
+//       var _this = this;
 
-      var oldText = this.el.innerText;
-      var length = Math.max(oldText.length, newText.length);
-      var promise = new Promise(function (resolve) {
-        return _this.resolve = resolve;
-      });
-      this.queue = [];
-      for (var i = 0; i < length; i++) {
-        var from = oldText[i] || '';
-        var to = newText[i] || '';
-        var start = Math.floor(Math.random() * 40);
-        var end = start + Math.floor(Math.random() * 80);
-        this.queue.push({ from: from, to: to, start: start, end: end });
-      }
-      cancelAnimationFrame(this.frameRequest);
-      this.frame = 0;
-      this.update();
-      return promise;
-    };
+//       var oldText = this.el.innerText;
+//       var length = Math.max(oldText.length, newText.length);
+//       var promise = new Promise(function (resolve) {
+//         return _this.resolve = resolve;
+//       });
+//       this.queue = [];
+//       for (var i = 0; i < length; i++) {
+//         var from = oldText[i] || '';
+//         var to = newText[i] || '';
+//         var start = Math.floor(Math.random() * 40);
+//         var end = start + Math.floor(Math.random() * 80);
+//         this.queue.push({ from: from, to: to, start: start, end: end });
+//       }
+//       cancelAnimationFrame(this.frameRequest);
+//       this.frame = 0;
+//       this.update();
+//       return promise;
+//     };
 
-    TextScramble.prototype.update = function update() {
-      var output = '';
-      var complete = 0;
-      for (var i = 0, n = this.queue.length; i < n; i++) {
-        var _queue$i = this.queue[i];
-        var from = _queue$i.from;
-        var to = _queue$i.to;
-        var start = _queue$i.start;
-        var end = _queue$i.end;
-        var char = _queue$i.char;
+//     TextScramble.prototype.update = function update() {
+//       var output = '';
+//       var complete = 0;
+//       for (var i = 0, n = this.queue.length; i < n; i++) {
+//         var _queue$i = this.queue[i];
+//         var from = _queue$i.from;
+//         var to = _queue$i.to;
+//         var start = _queue$i.start;
+//         var end = _queue$i.end;
+//         var char = _queue$i.char;
 
-        if (this.frame >= end) {
-          complete++;
-          output += to;
-        } else if (this.frame >= start) {
-          if (!char || Math.random() < 0.28) {
-            char = this.randomChar();
-            this.queue[i].char = char;
-          }
-          output += '<span class="dud">' + char + '</span>';
-        } else {
-          output += from;
-        }
-      }
-      this.el.innerHTML = output;
-      if (complete === this.queue.length) {
-        this.resolve();
-      } else {
-        this.frameRequest = requestAnimationFrame(this.update);
-        this.frame++;
-      }
-    };
+//         if (this.frame >= end) {
+//           complete++;
+//           output += to;
+//         } else if (this.frame >= start) {
+//           if (!char || Math.random() < 0.28) {
+//             char = this.randomChar();
+//             this.queue[i].char = char;
+//           }
+//           output += '<span class="dud">' + char + '</span>';
+//         } else {
+//           output += from;
+//         }
+//       }
+//       this.el.innerHTML = output;
+//       if (complete === this.queue.length) {
+//         this.resolve();
+//       } else {
+//         this.frameRequest = requestAnimationFrame(this.update);
+//         this.frame++;
+//       }
+//     };
 
-    TextScramble.prototype.randomChar = function randomChar() {
-      return this.chars[Math.floor(Math.random() * this.chars.length)];
-    };
+//     TextScramble.prototype.randomChar = function randomChar() {
+//       return this.chars[Math.floor(Math.random() * this.chars.length)];
+//     };
 
-    return TextScramble;
-  }();
+//     return TextScramble;
+//   }(); 
 
-  // ——————————————————————————————————————————————————
-  // Example
-  // ——————————————————————————————————————————————————
 
-  var phrases = ['Web Design', 'Graphic Design', 'Digital Art Direction', 'Front-End Web Development'];
 
-  var el = document.querySelector('.text');
-  var fx = new TextScramble(el);
+//   // ——————————————————————————————————————————————————
+//   // Example
+//   // ——————————————————————————————————————————————————
 
-  var counter = 0;
-  var next = function next() {
-    fx.setText(phrases[counter]).then(function () {
-      setTimeout(next, 3500);
-    });
-    counter = (counter + 1) % phrases.length;
-  };
+//   var phrases = ['Web Design', 'Graphic Design', 'Digital Art Direction', 'Front-End Web Development'];
 
-  next();
-}
-setTimeout(textScramble, 0);
+//   var el = document.querySelector('.text');
+//   var fx = new TextScramble(el);
+
+//   var counter = 0;
+//   var next = function next() {
+//     fx.setText(phrases[counter]).then(function () {
+//       setTimeout(next, 3500);
+//     });
+//     counter = (counter + 1) % phrases.length;
+//   };
+
+//   next();
+// }
+// setTimeout(textScramble, 0);
 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
@@ -132,11 +134,4 @@ var docStyle = document.documentElement.style;
 var transformProp = typeof docStyle.transform == 'string' ? 'transform' : 'WebkitTransform';
 var info = document.querySelector(".info");
 
-flkty.on( 'scroll', function () {
-  flkty.slides.forEach( function (slide, i) {
-    var img = imgs[i];
-    var x = ( slide.target + flkty.x ) * -1/18;
 
-    img.style[transformProp] = 'translateX(' + x + 'px) scale(1.1)';
-  } );
-} );
